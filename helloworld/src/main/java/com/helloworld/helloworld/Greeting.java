38e6906afa47d4a356b47c5c45dc9e0639a6bf50
haruka012 <h.shino0828@gmail.com>
2023-11-13 15:30:30 +0900

    世界の挨拶を追加

---
+++ helloworld/src/main/java/com/helloworld/helloworld/Greeting.java
@@ -0,0 +1,14 @@
+package com.helloworld.helloworld;
+
+public class Greeting {
+    private String country;
+    private String greeting;
+
+    public Greeting(String country, String greeting) {
+        this.country = country;
+        this.greeting = greeting;
+    }
+    public String getCountry(){return country;}
+
+    public String getGreeting(){return greeting;}
+}
--- helloworld/src/main/java/com/helloworld/helloworld/HelloWorldController.java
+++ helloworld/src/main/java/com/helloworld/helloworld/HelloWorldController.java
@@ -2,10 +2,20 @@ package com.helloworld.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class HelloWorldController {
     @GetMapping("/helloworld")
     public String helloworld(){return "Hello World!";}
 
+    @GetMapping("/worldgreeting")
+    public List<Greeting> worldgreetinglist(){
+    List<Greeting> greetings = List.of(
+            new Greeting("Japan","こんにちは"),
+            new Greeting("China","你好"),
+            new Greeting("USA","Hello"),
+            new Greeting("France","Bonjour"));
+    return greetings;
+    }
 }
